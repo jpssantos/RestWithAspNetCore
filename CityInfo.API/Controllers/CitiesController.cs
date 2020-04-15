eb6a4ec217566a7bd389443a4080a8b9da95f0d5
jpssantos <joaopaulosama@gmail.com>
2020-04-14 22:50:09 -0300

    add the data manipulation methods

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using CityInfo.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using NLog.Web.LayoutRenderers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,28 @@ namespace CityInfo.API.Controllers
     [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
+        private readonly ICityInfoRepository _cityRepository;
+
+        public CitiesController(ICityInfoRepository cityRepository)
+        {
+            _cityRepository = cityRepository ?? throw new ArgumentNullException(nameof(cityRepository));
+        }
+
         [HttpGet]
         public IActionResult GetCities()
         {
-            return Ok(CitiesDataStore.Current.Cities);
+            var cityEntities = _cityRepository.GetCities();
+            var results = new List<CityWithoutPointsOfInterestDto>();
+            foreach (var cityEntity in cityEntities)
+            {
+                results.Add(new CityWithoutPointsOfInterestDto
+                {
+                    Id = cityEntity.Id,
+                    Description = cityEntity.Description,
+                    Name = cityEntity.Name
+                });
+            }
+            return Ok(results);
         }
 
         [HttpGet("{id}")]
---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CityInfo.API.Context;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContex _context;
+
+        public CityInfoRepository(CityInfoContex context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            return _context.Cities.OrderBy(c => c.Name).ToList();
+        }
+
+        public City GetCity(int cityId, bool includePointsOfInterest)
+        {
+            if (includePointsOfInterest)
+            {
+                return _context.Cities.Include(c => c.PointsOfInterest).Where(c => c.Id == cityId).FirstOrDefault();
+
+            }
+            return _context.Cities.Where(c => c.Id == cityId).FirstOrDefault();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            return _context.PointsOfInterest.Where(p => p.CityId == cityId && p.Id == pointOfInterestId).FirstOrDefault();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterestForCity(int cityId)
+        {
+            return _context.PointsOfInterest.Where(p => p.Id == cityId).ToList();
+        }
+    }
+}
