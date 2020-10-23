01632f4318ce6957ebb2d95ddc5d903a24bef2b4
Fructuoso <victor@fructuoso.com.br>
2020-10-23 12:09:11 -0300

    Refatorando Startup \ Removendo binários da solulcao \ Atualizando README com informacoes sobre como compilar e rodar a aplicacao

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,65 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using CityInfo.API.Context;
+using CityInfo.API.Extensions;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json.Serialization;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace CityInfo.API
 {
     public class Startup
     {
-        private IConfiguration _configuration;
+        private readonly IConfiguration _Configuration;
 
         public Startup(IConfiguration configuration)
         {
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DesignPatternSamples", Version = "v1" });
+            });
+            #endregion
+
             services.AddMvc()
                 .AddMvcOptions(o =>
                 {
                     o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
                 });
-            //.AddJsonOptions(o =>
-            //{
-            //    if (o.SerializerSettings.ContractResolver != null)
-            //    {
-            //        var castedResolver = o.SerializerSettings.ContractResolver
-            //                               as DefaultContractResolver;
-            //        castedResolver.NamingStrategy = null;
-            //    }
-            //});
+
+            RegisterServices(services);
 #if DEBUG
-            services.AddTransient<IMailService, LocalMailService>();
-#else
-            services.AddTransient<IMailService, CloudMailService>();
+            RegisterServices_DEBUG(services);
 #endif
-            var connectionString = _configuration["connectionsStrings:cityInfoConnectionString"];
-            // services.AddDbContext<CityInfoContex>();
+
+            #region DbContext
+            var connectionString = _Configuration.GetConnectionString("cityInfoConnectionString");
             services.AddDbContext<CityInfoContex>(o =>
             {
                 o.UseSqlServer(connectionString);
             });
-            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+            #endregion
+
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            #region Swagger
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+            });
+            #endregion
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -72,6 +78,17 @@ namespace CityInfo.API
             app.UseStatusCodePages();
 
             app.UseMvc();
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IMailService, CloudMailService>();
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
+        }
+        private void RegisterServices_DEBUG(IServiceCollection services)
+        {
+            services.Replace<IMailService, LocalMailService>();
         }
     }
 }
